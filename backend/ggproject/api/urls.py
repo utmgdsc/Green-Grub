d183093bf2d321a7c3012617d3665af6c66eec06
Razeen <razeen_ali@yahoo.com>
2024-02-06 15:36:07 -0500

    added signup endpoint

--- backend/ggproject/api/urls.py
+++ backend/ggproject/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-
+from ggauth.views import SignupView
 
 from . import views
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('test/', views.hello, name='hello'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('signup/', SignupView.as_view(), name='signup'),
 ]
--- backend/ggproject/ggauth/views.py
+++ backend/ggproject/ggauth/views.py
@@ -1,3 +1,15 @@
+# views.py
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class SignupView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"username": user.username}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
