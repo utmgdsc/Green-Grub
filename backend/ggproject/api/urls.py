ab96b7249f8ea516c5b203ee4f743a216bb310eb
Razeen <razeen_ali@yahoo.com>
2024-03-07 01:53:33 -0500

    added view leaderboard api endpoint

--- backend/ggproject/api/urls.py
+++ backend/ggproject/api/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('friend_requests_sent/', friends_views.view_sent_requests, name='friend_requests'),
     path('friend_requests_received/', friends_views.view_received_requests, name='friend_requests_received'),
     path('view_friends_list/', friends_views.view_friends_list, name='view_friends'),
+    path('view_leaderboard/', report_views.view_leaderboard, name='view_leaderboard')
 ]
