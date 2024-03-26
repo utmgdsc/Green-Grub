1a6b5af12df2fb474da839f516f74eabbc582837
Adel Müürsepp <adel.muursepp@gmail.com>
2024-03-25 23:55:43 -0400

    Added images to assets and changed links in frontend

--- frontend/app/explore/ResultsScreen.tsx
+++ frontend/app/explore/ResultsScreen.tsx
@@ -8,15 +8,15 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import {PRIMARY_GREEN} from '../colors';
-import { BUTTON_BORDERRADIUS } from '../sizing';
+import {BUTTON_BORDERRADIUS} from '../sizing';
 
 const ResultsScreen = ({route, navigation}) => {
   const {correctAnswersCount} = route.params;
 
   const getImageSource = () => {
     return correctAnswersCount > 3
-      ? 'https://res.cloudinary.com/adelcloud/image/upload/v1711267434/trophy_bcru48.png'
-      : 'https://res.cloudinary.com/adelcloud/image/upload/v1711267647/failedtest_jtez9f.png';
+      ? 'http://127.0.0.1:8000/assets/trophy.png'
+      : 'http://127.0.0.1:8000/assets/failedtest.png';
   };
 
   return (
