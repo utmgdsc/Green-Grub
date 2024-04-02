105d5f087d0cb9cc46b61e5e979472f1b1a61a8d
Henrik S. Zimmermann <henrik.zimmermann@utoronto.ca>
2024-04-02 16:59:53 -0400

    [PROFILE-SUMMARY] Added extra info to profile summary

--- frontend/app/profile/ProfileSummary.tsx
+++ frontend/app/profile/ProfileSummary.tsx
@@ -3,7 +3,7 @@ import React, {PropsWithChildren} from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
 import Card from '../Card';
 import {useGetOtherUserQuery} from '../login/api';
-import {TEXT_LARGE} from '../sizing';
+import {TEXT_LARGE, TEXT_MEDIUM} from '../sizing';
 
 type ProfileSummaryProps = PropsWithChildren<{
   username: string;
@@ -38,11 +38,26 @@ export default function ProfileSummary({
     return null;
   }
 
+  const {extra_info: extra_info_} = user;
+  let extra_info = {...extra_info_};
+  const info = {
+    City: extra_info.city,
+    Country: extra_info.country,
+  };
+
   return (
     <Card>
       <View style={{width: '100%', alignItems: 'center'}}>
         <ProfileImage uri={user.extra_info.avatar_url} size={100} />
         <Text style={styles.usernameText}>{username}</Text>
+        <View style={{width: '100%', marginVertical: 30, gap: 10}}>
+          {Object.entries(info).map(([key, value]) => (
+            <View key={key} style={styles.fieldContainer}>
+              <Text style={styles.fieldHeaderText}>{key}</Text>
+              <Text style={styles.fieldText}>{value}</Text>
+            </View>
+          ))}
+        </View>
         {children}
       </View>
     </Card>
@@ -54,5 +69,20 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     fontSize: TEXT_LARGE,
     color: 'black',
+  },
+  fieldContainer: {
+    flexDirection: 'row',
+    width: '100%',
+  },
+  fieldHeaderText: {
+    fontSize: TEXT_MEDIUM,
+    color: 'black',
+    fontWeight: 'bold',
+    flex: 1,
+  },
+  fieldText: {
+    fontSize: TEXT_MEDIUM,
+    color: 'black',
+    flex: 1,
   },
 });
