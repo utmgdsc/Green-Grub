fa7bb98d2a60aafa44318377e23b0ff613fb6ed2
Razeen <razeen_ali@yahoo.com>
2024-04-16 22:27:49 -0400

    updated requirements.txt and changed jwt token

--- backend/ggproject/ggproject/settings.py
+++ backend/ggproject/ggproject/settings.py
@@ -59,7 +59,7 @@ REST_FRAMEWORK = {
 
 # Raz - 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=50),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=50),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
